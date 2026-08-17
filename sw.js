f4c04e969f0f3cbef9bc90ed11116ae477372bee
zarifin8891-spec <zarifin8891@gmail.com>
2026-08-17 08:43:54 +0700

    Add service worker for PWA

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'melati-mas-kunjungan-v1';
+const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+  event.respondWith(
+    fetch(request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+      return response;
+    }).catch(() => caches.match(request).then(cached => cached || caches.match('./index.html')))
+  );
+});
