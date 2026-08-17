c9a06c3e7d69fbb6bb280ec08c06f0fa4012a60e
zarifin8891-spec <zarifin8891@gmail.com>
2026-08-17 09:41:57 +0700

    Refresh PWA cache for new Melati Mas icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'melati-mas-kunjungan-v1';
+const CACHE_NAME = 'melati-mas-kunjungan-v2';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', event => {
