93cccdc63517921e261f53ae6ce8151cf6bf904b
zarifin8891-spec <zarifin8891@gmail.com>
2026-08-18 10:27:43 +0700

    Refresh PWA cache for user management

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'melati-mas-kunjungan-v8';
+const CACHE_NAME = 'melati-mas-kunjungan-v9';
 const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg', './auth-enhancements.js', './role-enhancements.js'];
 const ENHANCEMENT = '<script src="./auth-enhancements.js" defer></script><script src="./role-enhancements.js" defer></script>';
 
